35ca20a26d6e6b3a77c4657ce53085305fdac810
Alex Tseng <amtseng@stanford.edu>
2020-02-17 17:55:48 -0800

    Generate binary dataset with q-values and peak ranks

--- src/data/generate_ENCODE_DNase_binary_labels.sh
+++ src/data/generate_ENCODE_DNase_binary_labels.sh
@@ -1,25 +1,25 @@
 cline=$1
-indir=/users/amtseng/att_priors/data/interim/ENCODE_DNase/binary/$cline
-outdir=/users/amtseng/att_priors/data/processed/ENCODE_DNase/binary/labels/$cline
+indir=/users/amtseng/att_priors/data/raw/ENCODE_DNase/$cline
+outdir=/users/amtseng/att_priors/data/interim/ENCODE_DNase/binary/$cline
 
-mkdir -p $outdir/peaks
-taskfile=$outdir/labelgen_tasks.tsv
+mkdir -p $outdir
+taskfile=$outdir/$cline\_labelgen_tasks.tsv
 
 printf "Preparing the task list\n"
-printf "" > $taskfile
+printf "task\tnarrowPeak\tambig\n" > $taskfile
 
-for expid in `find $indir -name *.bed.gz -exec basename {} \; | awk -F "_" '{print $2}' | sort -u`
+for expid in `find $indir -name *.bed.gz -exec basename {} \; | awk -F "_" '{print $1}' | sort -u`
 do
-	ambibed=${indir}/${cline}_${expid}_ambiguous.bed.gz
-	optbed=${indir}/${cline}_${expid}_optimal.bed.gz
+	ambibed=${indir}/${expid}_${cline}_peaks-ambi.bed.gz
+	optbed=${indir}/${expid}_${cline}_peaks-idr.bed.gz
 
-	printf "${cline}_${expid}\t$optbed\t\t$ambibed\n" >> $taskfile
+	printf "${cline}_${expid}\t$optbed\t$ambibed\n" >> $taskfile
 done
 
 printf "Running label generator\n"
 
 genomewide_labels --task_list $taskfile \
-				  --outf $outdir/$cline\_labels.h5 \
+				  --outf $outdir/$cline\_genomewide_labels.h5 \
 				  --output_type hdf5 \
 				  --chrom_sizes /users/amtseng/genomes/hg38.canon.chrom.sizes \
 				  --bin_stride 50 \
@@ -29,8 +29,5 @@ genomewide_labels --task_list $taskfile \
 				  --chrom_threads 10 \
 				  --task_threads 4 \
 				  --allow_ambiguous \
+				  --save_label_source \
 				  --labeling_approach peak_percent_overlap_with_bin_classification
-
-# Cleanup
-rm -rf $outdir/peaks
-rm $taskfile
--- src/data/generate_ENCODE_TFChIP_binary_labels.sh
+++ src/data/generate_ENCODE_TFChIP_binary_labels.sh
@@ -1,14 +1,15 @@
 tfname=$1
 indir=/users/amtseng/att_priors/data/raw/ENCODE_TFChIP/$tfname/tf_chipseq
-outdir=/users/amtseng/att_priors/data/processed/ENCODE_TFChIP/binary/labels/$tfname
+outdir=/users/amtseng/att_priors/data/interim/ENCODE_TFChIP/binary/$tfname
+chromsizes=/users/amtseng/genomes/hg38.canon.chrom.sizes
 
 bglimit=150000
 
 mkdir -p $outdir/peaks
-taskfile=$outdir/labelgen_tasks.tsv
+taskfile=$outdir/$tfname\_labelgen_tasks.tsv
 
 printf "Preparing the task list\n"
-printf "" > $taskfile
+printf "task\tnarrowPeak\tambig\n" > $taskfile
 
 for expid in `find $indir -name *peaks*.bed.gz -exec basename {} \; | awk -F "_" '{print $1}' | sort -u`
 do
@@ -18,28 +19,27 @@ do
 	optacc=$(ls $indir/$expid\_$cline\_peaks-optimal* | xargs basename | cut -d "." -f 1 | cut -d "_" -f 4 | uniq)
 
 	bgbed=${indir}/${expid}_${cline}_peaks-all_${bgacc}.bed.gz
-	bgcanbed=${outdir}/peaks/${expid}_${cline}_peaks-all_${bgacc}.canon.bed.gz
+	bgtopbed=${indir}/${expid}_${cline}_peaks-all_${bgacc}.top.bed.gz
 	optbed=${indir}/${expid}_${cline}_peaks-optimal_${optacc}.bed.gz
-	optcanbed=${outdir}/peaks/${expid}_${cline}_peaks-optimal_${optacc}.canon.bed.gz
 	ambibed=${outdir}/peaks/${expid}_${cline}_ambig.bed
 
-	printf "\tLimiting background set to top $bglimit peaks, and keeping only canonical contigs\n"
-	zcat $bgbed | sort -k7,7rn | head -n $bglimit | awk '$1 ~ /^(chr1|chr2|chr3|chr4|chr5|chr6|chr7|chr8|chr9|chr10|chr11|chr11|chr12|chr13|chr14|chr15|chr16|chr17|chr18|chr19|chr20|chr21|chr22|chrX|chrY|chrM)$/' | gzip > $bgcanbed
-	zcat $optbed | awk '$1 ~ /^(chr1|chr2|chr3|chr4|chr5|chr6|chr7|chr8|chr9|chr10|chr11|chr11|chr12|chr13|chr14|chr15|chr16|chr17|chr18|chr19|chr20|chr21|chr22|chrX|chrY|chrM)$/' | gzip > $optcanbed
+	printf "\tLimiting background set to top $bglimit peaks\n"
+	# Sort in descending order by -log(q-value)
+	zcat $bgbed | sort -k9,9rn | head -n $bglimit | gzip > $bgtopbed
 
 	printf "\tGenerating the ambiguous blacklist for $cline\n"
 	# Generate the blacklist of ambiguous regions by taking the coordinates in
 	# the background that are not in the "optimal" set
-	bedtools intersect -a $bgcanbed -b $optcanbed -v > $ambibed
-	printf "${expid}_${cline}\t$optcanbed\t\t$ambibed\n" >> $taskfile
+	bedtools intersect -a $bgtopbed -b $optbed -v > $ambibed
+	printf "${expid}_${cline}\t$optbed\t$ambibed\n" >> $taskfile
 done
 
 printf "Running label generator\n"
 
 genomewide_labels --task_list $taskfile \
-				  --outf $outdir/$tfname\_labels.h5 \
+				  --outf $outdir/$tfname\_genomewide_labels.h5 \
 				  --output_type hdf5 \
-				  --chrom_sizes /users/amtseng/genomes/hg38.canon.chrom.sizes \
+				  --chrom_sizes $chromsizes \
 				  --bin_stride 50 \
 				  --bin_size 200 \
 				  --left_flank 400 \
@@ -47,8 +47,8 @@ genomewide_labels --task_list $taskfile \
 				  --chrom_threads 10 \
 				  --task_threads 4 \
 				  --allow_ambiguous \
+				  --save_label_source \
 				  --labeling_approach peak_percent_overlap_with_bin_classification
 
 # Cleanup
 rm -rf $outdir/peaks
-rm $taskfile
